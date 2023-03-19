9caba291dd86aab4979629b10a62ad3e780f591c
Avenjamin <avenjamin@users.noreply.github.com>
2023-03-20 00:47:36 +1100

    JS file typo take 2
    color vs colour

--- Public/js/module.js
+++ Public/js/module.js
@@ -1,6 +1,6 @@
 $('.theme-colours a').click(function(e) {
-	$("input[name='settings[themes.theme]']").val($(this).attr('data-color'));
-	$("#colour-label").html($(this).attr('data-color'));
+	$("input[name='settings[themes.theme]']").val($(this).attr('data-colour'));
+	$("#colour-label").html($(this).attr('data-colour'));
 	$('.theme-colours a').removeClass('active');
 	$(this).addClass('active');
 	e.preventDefault();
